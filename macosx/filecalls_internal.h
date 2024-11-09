5f8148f7a8b213e62065d7a0e598a0e008b5cab5
C.W. Betts <computers57@hotmail.com>
2024-11-08 17:03:51 -0700

    macOS updates: Warning, untested.

--- macosx/filecalls_internal.h
+++ macosx/filecalls_internal.h
@@ -7,11 +7,14 @@
 
 #include <sys/types.h>
 #include <dirent.h>
+#include <sys/syslimits.h>
 
 struct Directory_s {
   DIR *hDir;
   char *sPath;
   size_t sPathLen;
 };
+
+#define ARCEM_PATH_MAX PATH_MAX
 
 #endif /* __FILECALLS_H */
